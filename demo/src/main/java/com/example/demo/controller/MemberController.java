26826d4f32452fd49a96e1ded70ed091fa81d4cf
admin <eden20041228@gmail.com>
2024-12-04 12:27:54 +0900

    9주차 중간

---
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.domain.Article;
+import com.example.demo.model.service.AddArticleRequest;
+import com.example.demo.model.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.service.AddMemberRequest;
+
+@RequiredArgsConstructor
+@RestController // @Controller + @ResponseBody
+
+public class MemberController {
+    @GetMapping("/join_new") // 회원 가입 페이지 연결
+    public String join_new() {
+        return "join_new"; // .HTML 연결
+    }
+
+    @PostMapping("/api/members") // 회원 가입 저장
+    public String addmembers(@ModelAttribute AddMemberRequest request) {
+        memberService.saveMember(request);
+        return "join_end"; // .HTML 연결
+    }
+}
